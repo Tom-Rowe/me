c8713215ff64a582879f503b1a47c999988e5a39
Tom Rowe <tomrowingboat@gmail.com>
2019-08-01 21:37:44 +1000

    Part 1 got me confused because i had editted it in a workshop, so that took a while to figure out, the triangles i understand though

--- week5/exercise1.py
+++ week5/exercise1.py
@@ -14,43 +14,15 @@ Some functions will have directions as external comments, once you think you
 are on top of it, take these comments out. Others won't have comments and
 you'll need to figure out for yourself what to do.
 """
-
-
-# This is a terrible function. The rest of the functions in this file do a
-# much better job of what it's trying to do. Once you've has a little look,
-# move on, and eventually delete this function. (And this comment!)
-def do_bunch_of_bad_things():
-    countdown("Getting ready to start in", 9, 1, "Lets's go!")
-
-
-    triangle = {"base": 3, "height": 4}
-    triangle["hypotenuse"] = triangle["base"] ** 2 + triangle["height"] ** 2
-    print("area = " + str((triangle["base"] * triangle["height"]) / 2))
-    print("side lengths are:")
-    print("base: {}".format(triangle["base"]))
-    print("height: {}".format(triangle["height"]))
-    print("hypotenuse: {}".format(triangle["hypotenuse"]))
-
-    another_hyp = 5 ** 2 + 6 ** 2
-    print(another_hyp)
-
-    yet_another_hyp = 40 ** 2 + 30 ** 2
-    print(yet_another_hyp)
-
-
 # return a list of countdown messages, much like in the bad function above.
 # It should say something different in the last message.
 def countdown(message, start, stop, completion_message):
-    print("Getting ready to start in 9")
-    print("Getting ready to start in 8")
-    print("Getting ready to start in 7")
-    print("Getting ready to start in 6")
-    print("Getting ready to start in 5")
-    print("Getting ready to start in 4")
-    print("Getting ready to start in 3")
-    print("Getting ready to start in 2")
-    print("Getting ready to start in 1")
-    print("Let's go!")
+    countlist = []
+    #if stop==1:
+    for i in range(start-stop+1, stop-stop, -1):
+        print(message,str(i))
+    print(completion_message)
+    return countlist
 
 
 # TRIANGLES
@@ -63,32 +35,42 @@ def countdown(message, start, stop, completion_message):
 # The stub functions are made for you, and each one is tested, so this should
 # hand hold quite nicely.
 def calculate_hypotenuse(base, height):
-    pass
+    hypotenuse = (base**2 + height**2)**(1/2)
+    return(hypotenuse)
 
 
 def calculate_area(base, height):
-    pass
+    area = (base*height)/(2)
+    return(area)
 
 
 def calculate_perimeter(base, height):
-    pass
+    perimeter = (base*base + height*height)**(1/2) + base + height
+    return(perimeter)
 
 
 def calculate_aspect(base, height):
-    pass
+    aspect = ""
+    if height > base:
+        aspect = "tall"
+    elif base > height:
+        aspect = "wide"
+    else:
+        aspect = "equal"
+    return aspect
 
 
 # Make sure you reuse the functions you've already got
 # Don't reinvent the wheel
 def get_triangle_facts(base, height, units="mm"):
     return {
-        "area": None,
-        "perimeter": None,
-        "height": None,
-        "base": None,
-        "hypotenuse": None,
-        "aspect": None,
-        "units": None,
+        "area": calculate_area(base, height),
+        "perimeter": calculate_perimeter(base, height),
+        "height": height,
+        "base": base,
+        "hypotenuse": calculate_hypotenuse(base, height),
+        "aspect": calculate_aspect(base, height),
+        "units": units,
     }
 
 
@@ -140,6 +122,13 @@ def tell_me_about_this_right_triangle(facts_dictionary):
     )
 
     facts = pattern.format(**facts_dictionary)
+
+
+
+
+
+
+
 
 
 def triangle_master(base, height, return_diagram=False, return_dictionary=False):
